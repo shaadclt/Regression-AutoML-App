6e906f1c750c16a53342a7883cd5882a38d00dc3
Mohamed Shaad <98437584+shaadclt@users.noreply.github.com>
2023-06-16 09:03:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 from pycaret.regression import setup, compare_models, pull, save_model
+import pandas_profiling
 import pandas as pd
 from streamlit_pandas_profiling import st_profile_report
 import os 
